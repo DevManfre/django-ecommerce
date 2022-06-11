29e3c838a88beb6d2a759a4a4336d8b58cd7b62a
Alessio <94976747+DevManfre@users.noreply.github.com>
2022-06-11 11:31:07 +0200

    Visualizzazione del prodotto

--- app_prodotti/urls.py
+++ app_prodotti/urls.py
@@ -5,4 +5,5 @@ app_name = 'app_prodotti'
 
 urlpatterns = [
     path('productsList/', productsListView.as_view(), name='productsList'),
+    path('productDetails/<pk>/', productDetailsView.as_view(), name='productDetails'),
 ]
--- app_prodotti/views.py
+++ app_prodotti/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import *
 
 # Create your views here.
 class productsListView(ListView):
     model = Product
     template_name = 'productsList.html'
+
+class productDetailsView(DetailView):
+    model = Product
+    template_name = 'productDetails.html'
